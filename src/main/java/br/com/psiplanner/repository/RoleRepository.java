8188ace521cf7926039c0b3dbb3d0d10a624551b
Igor Marques <igorzig13@gmail.com>
2024-09-07 21:03:49 -0300

    Adds query methods to some repositories.

--- src/main/java/br/com/psiplanner/repository/RoleRepository.java
+++ src/main/java/br/com/psiplanner/repository/RoleRepository.java
@@ -2,6 +2,10 @@ package br.com.psiplanner.repository;
 
 import br.com.psiplanner.domain.auth.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Role findByName(String name);
 }
--- src/main/java/br/com/psiplanner/repository/UserRepository.java
+++ src/main/java/br/com/psiplanner/repository/UserRepository.java
@@ -2,6 +2,13 @@ package br.com.psiplanner.repository;
 
 import br.com.psiplanner.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
 }
